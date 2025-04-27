84bc7721507a97e1c0467d10bb8bcda8840b0b30
romaing <rocambille@gmail.com>
2025-04-27 17:14:14 +0200

    developed token structure with an id and left/right parts

--- src/react/pages/Home.tsx
+++ src/react/pages/Home.tsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
-const tokens = ["h1", "img"];
+const tokens = [
+  { id: 1, left: "<h1", right: ">" },
+  { id: 2, left: "<img", right: "/>" },
+];
+
+type Token = (typeof tokens)[0];
 
 function Home() {
-  const [code, setCode] = useState([] as string[]);
+  const [code, setCode] = useState([] as Token[]);
 
-  const addCode = (token: string) => setCode([...code, token]);
+  const addCode = (token: Token) => setCode([...code, token]);
 
   return (
     <>
       <h1>Home</h1>
       <p>Lorem Ipsum</p>
       <div>
-        {code.map((token) => (
-          <p key={token}>{token}</p>
+        {code.map((token, index, array) => (
+          <div key={token.id}>
+            <span>{token.left}</span>
+            {index === array.length - 1 && <span>{token.right}</span>}
+          </div>
         ))}
       </div>
       <div>
         {tokens.map((token) => (
-          <button key={token} type="button" onClick={() => addCode(token)}>
-            {token}
+          <button key={token.id} type="button" onClick={() => addCode(token)}>
+            {token.left} {token.right}
           </button>
         ))}
       </div>
